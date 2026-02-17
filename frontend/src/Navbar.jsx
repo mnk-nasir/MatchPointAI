8131aa8c211fccda5231797bd00c646261ab1cbb
mnk-nasir <Zafer.naseer@gmail.com>
2026-02-17 16:54:31 +0530

    up

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../Asset/logo.png";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="navbar">
       <div className="navbar-inner">
         <div className="navbar-left">
           <img src={logo} alt="MatchPoint logo" className="navbar-logo" />
-          {/* <span className="navbar-brand">MatchPoint</span> */}
         </div>
-        <nav className="navbar-links">
+        <button
+          className={`navbar-toggle${isMenuOpen ? " navbar-toggle-open" : ""}`}
+          type="button"
+          aria-label="Toggle navigation"
+          aria-expanded={isMenuOpen}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <nav
+          className={`navbar-links${isMenuOpen ? " navbar-links-open" : ""
+            }`}
+        >
           <a href="#advantage">Our Advantage</a>
           <a href="#features">Features</a>
           <a href="#testimonials">Testimonials</a>
           <a href="#solutions">Solutions</a>
           <a href="#faqs">Faqs</a>
+          <div className="navbar-right-mobile">
+            <button className="navbar-cta">Get In Touch</button>
+          </div>
         </nav>
         <div className="navbar-right">
           <button className="navbar-cta">Get In Touch</button>
